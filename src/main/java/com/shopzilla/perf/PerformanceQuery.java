eb0481a2414d8c2ffe3f4b80d9dd6bd8c75dbf49
Aditya Bhiday <aditya.bhiday@gmail.com>
2011-05-21 15:17:26 -0700

    First commit, project created using Roo

---
+++ src/main/java/com/shopzilla/perf/PerformanceQuery.java
@@ -0,0 +1,5 @@
+package com.shopzilla.perf;
+
+
+public interface PerformanceQuery {
+}
---
+++ src/test/java/com/shopzilla/perf/PerformanceQueryTest.java
@@ -0,0 +1,15 @@
+package com.shopzilla.perf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.springframework.mock.staticmock.MockStaticEntityMethods;
+
+@RunWith(JUnit4.class)
+@MockStaticEntityMethods
+public class PerformanceQueryTest {
+
+    @Test
+    public void testMethod() {
+    }
+}
